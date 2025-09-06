a7e7c119b4583a39f3a960a7c3dbad9a9d00b8f5
SidW111 <siddhantwainngade@gmail.com>
2025-09-06 17:01:55 +0530

    info

--- closure/Closure.js
+++ closure/Closure.js
@@ -1,3 +1,9 @@
+///closure is a feature in js.
+///where an inner function retains the access to the variables and the parameters
+///of its outer function (enclosing function)
+///even after the function has finished the execution
+///the ability of remembering its surrounding env is a key characteristics of the Closure
+
 function Outer() {
   const name = "Sid";
   function Inner() {
@@ -8,3 +14,4 @@ function Outer() {
 
 const Name = Outer();
 mainName();
+
--- lexical_scoping/lexical.js
+++ lexical_scoping/lexical.js
@@ -1,10 +1,22 @@
-function Outer(){
-    const name = 'sid'
-    function Inner(){
-        console.log(name)
-    }
-    Inner()
+///lexical scope in js is also known as a static scope determines the
+///accessibility of variables and functions based on their physical placement
+///within the source code at the time of definition
+
+///Inner functions have access to variables and functions defined in their parent (outer) scopes.
+///Outer scopes cannot access variables or functions defined within their inner (child) scopes. 
+
+///The scope chain moves upwards, not downwards.
+
+///Lexical scope is determined when the code is written (at compile time), not during runtime execution.
+///This contrasts with dynamic scope, which would depend on the runtime call stack.
+
+function Outer() {
+  const name = "sid";
+  function Inner() {
+    console.log(name);
+  }
+  Inner();
 }
 
-const name = Outer()
-console.log(name)
+const name = Outer();
+console.log(name);
