8fe79370112b61f4c3754b6ddffdf5d7fba57cda
SidW111 <siddhantwainngade@gmail.com>
2025-09-08 16:14:54 +0530

    info promises

--- promises/promise.js
+++ promises/promise.js
@@ -1,6 +1,8 @@
 //promise is an object in javascript
 //that represents the eventual completion(resolves) or failure(rejects)
 //of an async operation
+//promise is also a microtask but the fetch is given a priority or a special callback
+
 const promise = new Promise((resolve, reject) => {
   setTimeout(() => {
     resolve("done");
@@ -10,27 +12,33 @@ const promise = new Promise((resolve, reject) => {
 promise.then((data) => {
   console.log(data);
 });
-// async function getAllUsers() {
-//   const response = await fetch("https://jsonplaceholder.typicode.com/users");
-//   const data = await response.json();
-//   console.log(data);
-// }
 
-// getAllUsers();
+//async await is a syntactic sugar for the promises
+//it helps to understand the async code synchronously
+
+async function getAllUsers() {
+  const response = await fetch("https://jsonplaceholder.typicode.com/users");
+  const data = await response.json();
+  console.log(data);
+}
+
+getAllUsers();
 
 //fetch is a microtask and has a priority in callback
 //it's executed or stacked first in the callback queue or call stack
 //fetch will get resolved first
+//fetch is bundled with the node js itself back in 2022 officially by the node js
+//it has been built by the bluebird library
 
-// fetch("https://jsonplaceholder.typicode.com/users")
-//   .then((data) => {
-//     data.json().then((data) => {
-//       console.log(data);
-//     });
-//   })
-//   .catch((error) => {
-//     console.log(error);
-//   })
-//   .finally(() => {
-//     console.log("done");
-//   });
+fetch("https://jsonplaceholder.typicode.com/users")
+  .then((data) => {
+    data.json().then((data) => {
+      console.log(data);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  })
+  .finally(() => {
+    console.log("done");
+  });
