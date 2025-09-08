2b96e3785c9c841ffd7354d214eddacaabdf71bd
SidW111 <siddhantwainngade@gmail.com>
2025-09-08 13:16:11 +0530

    info

--- call_apply_bind/index.js
+++ call_apply_bind/index.js
@@ -4,9 +4,17 @@ function Greet(person, punctuation) {
 
 const person = { namee: "Sid" };
 
+
+//Invokes the function immediately 
+//takes arguments individually
 Greet.call(person, " Hello! ", "I'm from call");
 
+//Invokes the function immediately
+//takes arguments in an array
 Greet.apply(person, [" Hello! ", "I'm from apply"]);
 
+//does not invokes the function insteas returns a function
+//with bounded to the current context provided
+//usefull for callbacks
 const sid = Greet.bind(person, " Hello! ", "I'm from bind");
 sid();
