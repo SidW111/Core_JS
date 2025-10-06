190a4df08126603906b0282d77dc17566fe54d66
SidW111 <siddhantwainngade@gmail.com>
2025-10-06 17:32:24 +0530

    rest

---
+++ SpreadVsRest/index.js
@@ -0,0 +1,8 @@
+//Spread Operator is used to expand an iterable elements
+//(like an array or a string) into its individual elements. 
+
+const oldNumber  = [1,2,3]
+
+const newNumber = [...oldNumber]
+
+console.log(newNumber)
