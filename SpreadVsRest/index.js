136afe11fe0d191c9a6f2c63f8ca392a73cac38a
SidW111 <siddhantwainngade@gmail.com>
2025-10-06 21:57:19 +0530

    spread

--- SpreadVsRest/index.js
+++ SpreadVsRest/index.js
@@ -1,8 +1,58 @@
 //Spread Operator is used to expand an iterable elements
-//(like an array or a string) into its individual elements. 
+//(like an array or a string) into its individual elements.
 
-const oldNumber  = [1,2,3]
+//deep copy
+// const oldNumber = [1, 2, 3];
+// //deep copy
+// const newNumber = [...oldNumber];
 
-const newNumber = [...oldNumber]
+// oldNumber.push(4);
+// console.log(newNumber);
 
-console.log(newNumber)
+//merge two arrays
+
+// const order = {
+//     id:1,
+//     prodName:'Cube',
+//     customerName:'Sid',
+// }
+
+// const newOrder ={
+//     ...order
+// }
+
+// console.log(newOrder)
+
+//shallow copy of the object
+
+// const order = {
+//   id: 1,
+//   prodName: "Cube",
+//   customerName: "Sid",
+//   items: [1, 2, 3],
+// };
+// const newOrder = {
+//   ...order,
+// };
+// order.items.push(4);
+// console.log(newOrder);
+
+/*
+merge two objects
+*/
+
+// const user = {
+//   id: 1,
+//   name: "sid",
+// };
+
+// const address = {
+//   street: "main street",
+// };
+
+// const userName = {
+//   ...user,
+//   ...address,
+// };
+
+// console.log(userName);
