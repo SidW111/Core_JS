60fc497ac9703caa736c8c88b22e0e21cbf6e6f4
SidW111 <siddhantwainngade@gmail.com>
2025-10-07 15:35:26 +0530

    deep

--- object-array/shallowVSdeep/deep.js
+++ object-array/shallowVSdeep/deep.js
@@ -2,27 +2,31 @@
 //a deep copy in contrast creates a completely independent
 //duplicate of the original object or array, including all
 //nested objects and arrays.
+
 //It allocates new memory for every part of the structure
 //ensuring that changes made to the copied will not
 //affect the og and vice versa
 
 const obj = { 1: "sid", 2: "savior" };
 
-const copyObj = structuredClone(obj);  //modern way for deep copy
-const copyObject = JSON.parse(JSON.stringify(obj));  //A common method for deep copy
+const deepcopyModern = structuredClone(obj);  //modern way for deep copy
+const deepcopyTraditional = JSON.parse(JSON.stringify(obj));  //A common method for deep copy
 
-copyObj[1] = "siddhant";
-obj[2] = "waingade";
-console.log(obj);
-console.log(copyObj);
-console.log(copyObject)
+//this change will only reflect in the og object
+obj[1] = 'Siddhant'
+console.log(obj)
 
-const arr = [1,2,3,4,5]
-const copyArr = structuredClone(arr) // modern way for deep copy
-const copyArray = JSON.parse(JSON.stringify(arr)) //A common method for deep copy
+//both the deep copies will not show all this updates of the og array
+console.log(deepcopyModern);
+console.log(deepcopyTraditional)
 
-arr[2] = 22
-copyArr[3] = 33
-console.log(arr)
-console.log(copyArr)
-console.log(copyArray)
+
+// const arr = [1,2,3,4,5]
+// const copyArr = structuredClone(arr) // modern way for deep copy
+// const copyArray = JSON.parse(JSON.stringify(arr)) //A common method for deep copy
+
+// arr[2] = 22
+// copyArr[3] = 33
+// console.log(arr)
+// console.log(copyArr)
+// console.log(copyArray)
