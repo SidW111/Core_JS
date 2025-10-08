327b3d96c0a7907b460223d1a13c2612dae0d920
SidW111 <siddhantwainngade@gmail.com>
2025-10-08 16:47:51 +0530

    HOFs

---
+++ functions/09_higher_order_functions/index.js
@@ -0,0 +1,14 @@
+/**
+ * HIGHER ORDER FUNCTIONS (HOFs)
+ */
+
+
+// Higher order functions are functions that either satisfies 
+// one of the following conditions
+// 1. Accepts one or more function as arguments (known as callbacks).
+// 2. Returns a function as their result
+
+//Since the functions in js as treated as a first class citizens.
+//meaning they can be assigned to the variable, passed as an argument, and returned.
+//they can be easily used to create HOFs
+
