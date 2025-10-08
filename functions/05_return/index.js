27cc8b6c862e9863e069dd429eb4429fbf3ca381
SidW111 <siddhantwainngade@gmail.com>
2025-10-08 13:41:41 +0530

    return

---
+++ functions/05_return/index.js
@@ -0,0 +1,24 @@
+/**
+ * RETURN keyword
+ */
+
+//The return keyword in JavaScript is used within a function to perform two primary actions
+
+/*
+ *1:- Terminate Function Execution
+ */
+
+//When the return statement is encountered inside a function,
+//the function's execution is immediately stops.
+//Any code written after the return statement within the same function will not be executed.
+
+/*
+ *2:- Return A value
+ */
+
+//The return keyword can be followed by an expression or a value.
+//This value is sent back to the caller of the function
+//This returned value can be of any datatype.
+//Including Primitive or Non-Primitive datatypes.
+//If no value is explicitly specified after return, the function
+//implicitly returns undefined.
